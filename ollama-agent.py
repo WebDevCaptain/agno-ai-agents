29ba59c41ba910d2047901d0524e70f9de618a79
Shreyash <hello@webdevcaptain.com>
2025-03-03 20:13:08 +0530

    Streaming responses

--- ollama-agent.py
+++ ollama-agent.py
@@ -1,7 +1,8 @@
 from agno.agent import Agent
 from agno.models.ollama import Ollama
 
+agent = Agent(model=Ollama(id="deepseek-r1:8b"))
 
-agent = Agent(model=Ollama(id='deepseek-r1:8b'))
-
-agent.print_response("Write a paragraph in 100 words about the future of finance")
+agent.print_response(
+    "Write a paragraph in 100 words about the future of finance", stream=True
+)
